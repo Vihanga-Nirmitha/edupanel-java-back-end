eba242e7511a22aba1e5f3cd02fc6d1a460827be
Vihanga Nirmitha <vihanganirmitha200@gmail.com>
2023-12-04 19:04:54 +0530

    Finished Create new lecture web service

--- src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebAppConfig.java
@@ -1,15 +1,37 @@
 package lk.ijse.dep11.edupanel;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class WebAppConfig  {
+    @Bean
+    public Bucket defaultBucket() throws IOException {
+        InputStream serviceAccount =getClass().getResourceAsStream("/edupanel-56b7a-firebase-adminsdk-l3ex9-c9bd0a89b6.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setStorageBucket("edupanel-56b7a.appspot.com")
+                .build();
+
+        FirebaseApp.initializeApp(options);
+        return StorageClient.getInstance().bucket();
+
+    }
 @Bean
     public StandardServletMultipartResolver multipartResolver(){
         return new StandardServletMultipartResolver();
--- src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
+++ src/main/java/lk/ijse/dep11/edupanel/WebRootConfig.java
@@ -1,15 +1,24 @@
 package lk.ijse.dep11.edupanel;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.StorageClient;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 @Configuration
 @PropertySource("classpath:/applicaton.properties")
 public class WebRootConfig {
-    @Bean
+
+    @Bean(destroyMethod = "close")
     public HikariDataSource dataSource(Environment env){
         HikariDataSource config=new HikariDataSource();
         config.setJdbcUrl(env.getRequiredProperty("spring.datasource.url"));
